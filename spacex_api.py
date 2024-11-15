ecaa9732b8dd9d557685cadd32fc4b1ff3cc0745
MagicalPeach <andy.g.zhu@gmail.com>
2024-11-14 23:20:34 -0800

    git commit

--- spacex_api.py
+++ spacex_api.py
@@ -10,3 +10,16 @@ data = response.json()
 #Put into dataframe and extracts top 5 rows
 df = pd.DataFrame(data)
 print(df.head())
+
+#using pandas only for API fetching
+url = 'http://api.spacexdata.com/v4/launches'
+
+#directly moving API fetch into dataframe with Pandas
+df = pd.read_json(url)
+
+#extract first 5 rows
+print(df.head())
+
+#show column and data type values
+print(df.columns)
+print(df.dtypes)
